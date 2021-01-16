88c0d0bc07736afc256b8abbb278f569c1fa9694
Bahadir27 <b.ucer27@gmail.com>
2021-01-16 19:56:07 +0100

    labels added

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-more private
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+# create the canvas, size in pixels
+canvas = Canvas(width=200, height=200, bg='white')
+
+# pack the canvas into a frame/form
+canvas.grid(row=0, column=1)
+
+# load the .gif image file
+logo_image = PhotoImage(file='logo.png')
+
+# put gif image on canvas
+# pic's upper left corner (NW) on the canvas is at x=50 y=10
+canvas.create_image(100, 100, image=logo_image)
+
+
+#Labels
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
+
+
+window.mainloop()
